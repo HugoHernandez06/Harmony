fc6221d60ba6b67f93c5cf875d20c376d832a2cf
HugoHernandez06 <44584564+HugoHernandez06@users.noreply.github.com>
2021-03-11 17:22:45 -0500

    Procedimientos JS

--- js/localstorage.js
+++ js/localstorage.js
@@ -1,45 +1,26 @@
-
 function store(){
-
-    var name = document.getElementById('username');
+    var nameU = document.getElementById('nameU');
     var pw = document.getElementById('pw');
-    var numbers = /[0-9]/g;
-
-    if(name.value.length == 0){
-      //  alert('Por favor ingrese un email');
-
-    }else if(pw.value.length == 0){
-        //alert('Por favor ingrese una contraseña');
-    }
-    else if(name.value.length == 0 && pw.value.length == 0){
-      //  alert('Por favor ingrese un email & una contraseña');
-
-    }
-    else if(!pw.value.match(numbers)){
-       // alert('please add 1 number');
-
-    }
-    else{
-        localStorage.setItem('username', name.value);
-        localStorage.setItem('pw', pw.value);
-        //alert('Cuenta creada!!');
-    }
+    localStorage.setItem('nameU',nameU.value);
+    localStorage.setItem('pw',pw.value);
 }
-
 //checking
 function check(){
-    var storedName = localStorage.getItem('username');
+    // Register-Form
+    var storedName = localStorage.getItem('nameU');
     var storedPw = localStorage.getItem('pw'); 
     
+    // Login-Form
     var userName = document.getElementById('userName');
     var userPw = document.getElementById('userPw');
     
+    // Check
     if(userName.value == storedName && userPw.value == storedPw){
-        btn_home.addEventListener("click",()=>{irA(login);});
-       // alert('Estas logeado!!');
+        alert('Estas logeado!!');
+        btn_home.addEventListener("click",()=>{irA(home);});
     }
     else{
-        //btn_home.addEventListener("click",()=>{irA(login);});
-        //alert('Error en el login');
+        alert('Error en el login');
+        btn_login.addEventListener("click",()=>{irA(login);});
     }
 }
